c968e33a8a93151b59261d13dbe793cadd60619b
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2021-07-30 16:22:42 -0300

    KleinGordon: Included reflecting boundary in the LHS boundary routine

--- KleinGordon/src/Boundary.c
+++ KleinGordon/src/Boundary.c
@@ -53,5 +53,16 @@ void KleinGordon_RHSBoundaries(CCTK_ARGUMENTS) {
 void KleinGordon_Boundaries(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS;
   DECLARE_CCTK_PARAMETERS;
-  // Do nothing
+
+  if (CCTK_EQUALS(bc_type, "reflecting")) {
+
+	CCTK_LOOP3_INTBND(loop_reflecting, cctkGH, i, j, k, ni, nj, nk) {
+	  const CCTK_INT ijk = CCTK_GFINDEX3D(cctkGH, i, j, k);
+	  Phi[ijk] = 0.0;
+	  K_Phi[ijk] = 0.0;
+	}
+	CCTK_ENDLOOP3_INTBND(loop_reflecting);
+  } else {
+	// Do nothing
+  }
 }
